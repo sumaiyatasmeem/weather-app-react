5dcb1edfe35661845fd53c6e5d9a5406aec1f21b
Sumaiya Tasmeem <sumaiyatasmeem@gmail.com>
2020-05-04 19:16:14 +0600

    Updates

---
+++ precache-manifest.50b49e8889e5b8302239d01e5c23fcc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb9238f2179df6c28cd96ca4819efc3",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "8be8b389b05fccdbf291",
+    "url": "/weather-app-react/static/css/2.0cc2d472.chunk.css"
+  },
+  {
+    "revision": "6ba0d80f6d587541d1a6",
+    "url": "/weather-app-react/static/css/main.3fb4b920.chunk.css"
+  },
+  {
+    "revision": "8be8b389b05fccdbf291",
+    "url": "/weather-app-react/static/js/2.6ec6f348.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.6ec6f348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba0d80f6d587541d1a6",
+    "url": "/weather-app-react/static/js/main.c14457fc.chunk.js"
+  },
+  {
+    "revision": "b9e9c7f836084ebd178e",
+    "url": "/weather-app-react/static/js/runtime-main.d9f530f6.js"
+  }
+]);
